9aa98cb519e211350c9e7c81eed4b383417df06b
axel_j <axist8@gmail.com>
2021-04-25 22:55:41 -0700

    replicated portfolio header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
-import Hero from './Components/Hero/Hero';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Hero />
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,16 +3,25 @@ import Navbar from '../Navbar/Navbar';
 
 function Header() {
     return (
-        <div>
-            <div className="fullscreen">
-                <div class="halfscreen">
-                    <p>&lt;Axel Jorgensen | web developer&gt;</p>
-                </div>
-                <div class="halfscreen">
-                    <Navbar />
-                </div>
+        <header>
+        <div class="content-wrap">
+          <div class="thehead">
+            <div class="titlename">
+              <p class="axe">&lt;Axel Jorgensen | web developer&gt;</p>
             </div>
+            <div class="navi">
+              <Navbar />
+            </div>
+          </div>
         </div>
+        <div class="hero-wrap">
+          <div class="hero">
+            <div class="hero-in">
+              <h1><br /><br /><br /><br /><br /><br /><br />Let's &nbsp;&nbsp;<span>code</span><br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;something&nbsp;&nbsp; <span>cool</span></h1>
+            </div>
+          </div>
+        </div>
+      </header>
     )
 }
 
